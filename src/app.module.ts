2ffc3745ef4cdfd8a6c667f9d566146c82b58a19
Artem Korchunov <artem.korchunof@gmail.com>
2019-04-30 08:17:25 +0300

    Initial

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [UsersModule, TypeOrmModule.forRoot(), AuthModule],
+  controllers: [AppController],
+  providers: [AppService, UsersService],
+})
+export class AppModule {}
